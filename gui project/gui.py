07b139a6a883a5af07e4f243e3683996334c740b
anticlown322 <karasandrey2005@gmail.com>
2023-10-28 15:51:45 +0300

    refactor: change gui buttons and features

--- gui project/gui.py
+++ gui project/gui.py
@@ -3,6 +3,8 @@ import tkinter
 
 # consts start #
 software_version = "v1.0"
+
+
 # consts end #
 
 
@@ -10,9 +12,9 @@ software_version = "v1.0"
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-# App class start #
+
+
+# App class end #
 
 
 # app configs start #
@@ -21,101 +23,153 @@ customtkinter.set_default_color_theme("green")
 
 app = App()
 app.title(f"Обработчик сообщений {software_version}")
-app.geometry("800x620+600+300")
+w = app.winfo_screenwidth() // 2 - 400
+h = app.winfo_screenheight() // 2 - 310
+app.geometry(f"800x620+{w}+{h}")
 app.resizable(False, False)
 app.iconbitmap("images/circuit.ico")
+
+my_font_normal = customtkinter.CTkFont(family="JetBrains Mono", size=14, weight="normal")
+my_font_bold = customtkinter.CTkFont(family="JetBrains Mono", size=14, weight="bold")
 # app configs end #
 
 
 # panels config start #
 
-#       status frame start         #
-port_status_frame = customtkinter.CTkFrame(master=app,
-                                           width=200,
-                                           height=600,
-                                           corner_radius=10)
-port_status_frame.place(x=10, y=10)
-
-label_frame_name = customtkinter.CTkLabel(master=port_status_frame,
-                                          text="Состояние порта",
-                                          width=120,
-                                          height=25,
-                                          corner_radius=8)
-label_frame_name.grid(row=0, column=0)
-
-label_port_name = customtkinter.CTkLabel(master=port_status_frame,
-                                         text="Имя порта: ",
-                                         width=120,
-                                         height=25,
-                                         corner_radius=8)
-label_frame_name.grid(row=1, column=0)
-
-label_is_port_open = customtkinter.CTkLabel(master=port_status_frame,
-                                            text="Доступ к порту: ",
-                                            width=120,
-                                            height=25,
-                                            corner_radius=8)
-label_frame_name.grid(row=2, column=0)
-#       status frame end            #
-
-
-#       console frame start        #
-port_console_frame = customtkinter.CTkFrame(master=app,
-                                            width=350,
-                                            height=600,
-                                            corner_radius=10)
-port_console_frame.place(x=220, y=10)
-
-tabView = customtkinter.CTkTabview(port_console_frame,
-                                   width=330,
-                                   height=580)
-tabView.place(x=10, y=10)
-tabView.add("Ввод")
-tabView.add("Вывод")
-
-button_get_info = customtkinter.CTkButton(master=tabView.tab("Вывод"), text="Получить информацию")
-button_get_info.place(relx=0.5, rely=0.05, anchor=tkinter.CENTER)
-
-button_send_info = customtkinter.CTkButton(master=tabView.tab("Ввод"), text="Послать информацию")
-button_send_info.place(relx=0.5, rely=0.05, anchor=tkinter.CENTER)
-
-#               textBoxes config start              #
-textbox_input = customtkinter.CTkTextbox(tabView.tab("Ввод"), width=330, height=550)
-textbox_input.place(x=5, rely=0.1)
-welcome_text = " > Введите команду"
-textbox_input.insert("0.0", welcome_text)
-
-textbox_output = customtkinter.CTkTextbox(tabView.tab("Вывод"), width=330, height=550)
-textbox_output.place(x=5, rely=0.1)
-textbox_output.configure(state="normal")
-#               textBoxes config end                   #
-
-#       tabview config end          #
-
 
 #       buttons frame start         #
 buttons_frame = customtkinter.CTkFrame(master=app,
-                                       width=210,
-                                       height=600,
+                                       width=200,
+                                       height=200,
                                        corner_radius=10)
-buttons_frame.place(x=580, y=10)
+buttons_frame.place(x=10, y=10)
 
 buttons_label = customtkinter.CTkLabel(master=buttons_frame,
                                        text="Действия",
                                        width=120,
                                        height=25,
-                                       corner_radius=8)
-buttons_label.grid()
-# buttons_label.grid(row=0,rowspan=10)
+                                       corner_radius=8,
+                                       font=my_font_bold)
+buttons_label.place(relx=0.5, y=15, anchor=tkinter.CENTER)
 
-button_check_ports = customtkinter.CTkButton(master=buttons_frame, text="Узнать порты")
-button_check_ports.grid()
-# button_check_ports.grid(row=1,rowspan=10)
+button_check_ports = customtkinter.CTkButton(master=buttons_frame,
+                                             text="Обновить",
+                                             font=my_font_normal)
+button_check_ports.place(relx=0.5, y=45, anchor=tkinter.CENTER)
 
-button_choose_port = customtkinter.CTkButton(master=buttons_frame, text="Выбрать порт")
-button_choose_port.grid()
-# button_choose_port.grid(row=2,rowspan=10)
+button_choose_port = customtkinter.CTkButton(master=buttons_frame,
+                                             text="Настроить",
+                                             font=my_font_normal)
+button_choose_port.place(relx=0.5, y=85, anchor=tkinter.CENTER)
+
+button_spec_input = customtkinter.CTkButton(master=buttons_frame,
+                                            text="Спец.ввод",
+                                            font=my_font_normal)
+button_spec_input.place(relx=0.5, y=125, anchor=tkinter.CENTER)
 #       buttons frame end           #
+
+
+#       status frame start         #
+port_status_frame = customtkinter.CTkFrame(master=app,
+                                           width=200,
+                                           height=390,
+                                           corner_radius=10)
+port_status_frame.place(x=10, y=220)
+
+label_frame_name = customtkinter.CTkLabel(master=port_status_frame,
+                                          text="Состояние порта",
+                                          width=120,
+                                          height=25,
+                                          corner_radius=8,
+                                          font=my_font_bold)
+label_frame_name.place(relx=0.5, rely=0.06, anchor=tkinter.CENTER)
+
+label_port_name = customtkinter.CTkLabel(master=port_status_frame,
+                                         text="Имя порта: ",
+                                         font=my_font_normal,
+                                         justify="left")
+label_port_name.place(x=5, y=40)
+
+option_menu_var = customtkinter.StringVar(value="Нет")
+combobox_port_name = customtkinter.CTkComboBox(master=port_status_frame,
+                                               width=100,
+                                               values=[],
+                                               variable=option_menu_var)
+combobox_port_name.place(x=90,y=40)
+
+label_is_port_open = customtkinter.CTkLabel(master=port_status_frame,
+                                            text="Доступ к порту: ",
+                                            font=my_font_normal,
+                                            justify="left")
+label_is_port_open.place(x=5, y=70)
+
+label_port_configs = customtkinter.CTkLabel(master=port_status_frame,
+                                            text="Настройки порта",
+                                            font=my_font_bold)
+label_port_configs.place(relx=0.5, y=110, anchor=tkinter.CENTER)
+
+label_baudrate = customtkinter.CTkLabel(master=port_status_frame,
+                                        text="Скорость: ",
+                                        font=my_font_normal,
+                                        justify="left")
+label_baudrate.place(x=5, y=130)
+
+label_byte_size = customtkinter.CTkLabel(master=port_status_frame,
+                                         text="Размер байта: ",
+                                         font=my_font_normal,
+                                         justify="left")
+label_byte_size.place(x=5, y=160)
+
+label_parity_bit = customtkinter.CTkLabel(master=port_status_frame,
+                                          text="Бит четности: ",
+                                          font=my_font_normal,
+                                          justify="left")
+label_parity_bit.place(x=5, y=190)
+
+label_stop_bits = customtkinter.CTkLabel(master=port_status_frame,
+                                         text="Стоп-биты: ",
+                                         font=my_font_normal,
+                                         justify="left")
+label_stop_bits.place(x=5, y=220)
+#       status frame end            #
+
+
+#       console frame start        #
+port_console_frame = customtkinter.CTkFrame(master=app,
+                                            width=570,
+                                            height=600,
+                                            corner_radius=10)
+port_console_frame.place(x=220, y=10)
+
+tabView = customtkinter.CTkTabview(port_console_frame,
+                                   width=550,
+                                   height=580)
+tabView.place(x=10, y=10)
+tabView.add("Ввод")
+tabView.add("Вывод")
+
+button_get_info = customtkinter.CTkButton(master=tabView.tab("Вывод"), text="Получить информацию", font=my_font_bold)
+button_get_info.place(relx=0.5, rely=0.05, anchor=tkinter.CENTER)
+
+button_send_info = customtkinter.CTkButton(master=tabView.tab("Ввод"), text="Послать информацию", font=my_font_bold)
+button_send_info.place(relx=0.5, rely=0.05, anchor=tkinter.CENTER)
+
+#               textBoxes config start              #
+textbox_input = customtkinter.CTkTextbox(tabView.tab("Ввод"),
+                                         width=530,
+                                         height=550)
+textbox_input.place(x=5, rely=0.1)
+welcome_text = " > Введите команду"
+textbox_input.insert("0.0", welcome_text)
+
+textbox_output = customtkinter.CTkTextbox(tabView.tab("Вывод"),
+                                          width=530,
+                                          height=550)
+textbox_output.place(x=5, rely=0.1)
+textbox_output.configure(state="normal")
+#               textBoxes config end                   #
+
+#       console config end          #
 
 # panels config end #
 
